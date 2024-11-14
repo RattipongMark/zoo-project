a2da511d116c933fdb146ae429eced66cf1dbecd
Rattipong Sakunjeen (Mark) <rattipong.mark@gmail.com>
2024-11-14 04:23:25 +0000

    fixZoo and AddTestCase

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,21 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_invalid_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'Invalid Age')
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+    
+    def test_young_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
        
-    # Add your additional test cases here.
 
 if __name__ == '__main__':
     unittest.main()
